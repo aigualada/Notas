d463d15fe41f0b03406f7ec620a1e3666a9e10fd
adolfo <adolfo.igualada@gmail.com>
2017-07-10 11:05:50 +0200

    Se añade doctrine-fixtures-bundle y faker para cargar notas de prueba

---
+++ src/AppBundle/DataFixtures/ORM/LoadNotas.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Nota;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadNotas implements FixtureInterface
+{
+
+    const NUM_NOTAS = 20;
+
+    protected $faker;
+
+    public function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        for($i = 0; $i < self::NUM_NOTAS; $i++)
+        {
+            $nota = new Nota();
+            $nota->setTexto($this->faker->text(140));
+            $nota->setFavorita($this->faker->boolean(30));
+
+            $manager->persist($nota);
+
+        }
+
+        $manager->flush();
+    }
+
+
+}
